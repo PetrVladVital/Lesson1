85466bd7e25e7981974217ffb74e32e08ffefbea
PetrVladVital <vlsmpetr@gmail.com>
2022-08-26 14:00:48 +0300

    fact

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class Factorial {
+public class Factorial {
+    int fact (int n) {
+        int result;
+        if (n==1) return  1;
+        result = fact(n-1) * n;
+        return result;
+    }
 }
--- src/Recursion.java
+++ src/Recursion.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Recursion {
+public class Recursion {
+    public static void main(String[] args) {
+        Factorial f = new  Factorial();
+        System.out.println("factorial 5 =" + f.fact(5));
+    }
 }
